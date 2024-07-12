fa99c479b264fa5b2dbceb9d18e2f375459b2f31
Pablo <pablo.mrtnz23@gmail.com>
2024-07-12 20:21:08 -0300

    Modificación front (html) y librosModels.js

--- crud-api/models/librosModel.js
+++ crud-api/models/librosModel.js
@@ -8,6 +8,8 @@ const {DataTypes} = require("sequelize")
 const librosModel = db.define ("libros",{
     titulo:{type:DataTypes.STRING},
     isbn:{type:DataTypes.STRING},
+    autor_id:{type:DataTypes.INTEGER},
+    editorial_id:{type:DataTypes.INTEGER},
     anio_publicacion:{type:DataTypes.INTEGER},
     genero:{type:DataTypes.STRING},
     disponible:{type:DataTypes.BOOLEAN}
